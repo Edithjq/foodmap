42b4e48916b2077dc78b86819d9648d88918e4d9
edithjq <edithjq.21@gmail.com>
2018-01-02 10:12:40 -0800

    arreglando galeria restaurant

--- js/data.js
+++ js/data.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
 
-	var carnes = {"personas": [
-	{"nombre":"jorge","edad":23},
-	{"nombre":"../assets/img/postre3.jpg","edad":20}
-]};
-$("#content").text(carnes.personas[1].nombre + " " + carnes.personas[0].nombre);
+	var restaurant = {"carnes": [
+	{"nombre":"La Cuadra de Salvador Steakhouse","img":23},
+	{"nombre":"Baco y Vaca Restaurante","edad":20},
+	{"nombre":"Restaurante La Carreta","edad":20},
+	{"nombre": "Carnal Prime Steakhouse", "edad":12},
+	{"nombre": "Rustica San-Miguel", "edad":12},
+	{"nombre": "La Vista - JW Marriott", "edad":12}
+	]};
+
+$("#restaurant").text(carnes.personas[1].nombre + " " + carnes.personas[0].nombre);
 
 
 });
 
- 
+    
--- js/principal.js
+++ js/principal.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
 
 	var $search = $("#search");
 
-	$search.on("keyup", function(event){
+	$search.on("input", function(event){
 		event.preventDefault();
-		// obtener el valo   r
+		// obtener el valor
 		 console.log($(this).val()); 
+		 var inputSearch = $(this).val();
 
 		// if (img == $buscar) {
 			
